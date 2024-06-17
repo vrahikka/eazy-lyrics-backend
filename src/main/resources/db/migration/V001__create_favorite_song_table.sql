26b894a4396d0e08d3948d06ac17a24e9eecc504
Ville Rahikka <ville.rahikka@nitor.com>
2024-06-17 12:30:20 +0300

    update migration

--- src/main/resources/db/migration/V001__create_favorite_song_table.sql
+++ src/main/resources/db/migration/V001__create_favorite_song_table.sql
@@ -5,5 +5,6 @@ CREATE TABLE favorite_song(
     title text NOT NULL,
     artist_name text NOT NULL,
     thumbnail_url text NOT NULL,
-    CONSTRAINT favorite_song_pk PRIMARY KEY (id)
+    CONSTRAINT favorite_song_pk PRIMARY KEY (id),
+    CONSTRAINT unique_song_email_combination UNIQUE (song_id, email)
 );
