85f39900d0a307cbedda990456f34a1f40a78f87
Ville Rahikka <ville.rahikka@nitor.com>
2024-06-25 15:39:33 +0300

    Remove roles table

--- src/main/resources/db/migration/V004__create_user_roles_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE user_roles(
-    user_id BIGINT NOT NULL,
-    role_id BIGINT NOT NULL,
-    CONSTRAINT user_roles_pk PRIMARY KEY (user_id, role_id),
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id),
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id)
-);
